d3180752b5af38c7d73d9a67419f040feada51dd
Eric Cheatham <ericjcheatham@gmail.com>
2018-04-27 15:47:50 -0500

    Format CLI output

--- weather/cli.py
+++ weather/cli.py
@@ -1,15 +1,59 @@
+import argparse
+
 from weather import fetch_weather
+
 
 __all__ = [
     'main',
 ]
 
 CITY_PROMPT = 'Where are you? '
+REPORT_FORMAT = '{} weather:\n {} degrees {}'
+
+UNITS = {
+    'metric': 'Celsius',
+    'imperial': 'Fahrenheit',
+    'kelvin': 'Kelvin'
+}
+
+
+def get_arg_parser():
+    """
+    Command arguments
+    """
+    parser = argparse.ArgumentParser(
+        prog='weather',
+        description='Python based temperature app'
+    )
+
+    parser.add_argument(
+        '--format',
+        choices=['metric', 'kelvin', 'imperial']
+    )
+
+    return parser
+
 
 def main():
+    """
+    Command line entrypoint
+    """
+    parser = get_arg_parser()
+
+    args = parser.parse_args()
+
     while True:
         location = raw_input(CITY_PROMPT)
-        print(fetch_weather(location))
+        weather = fetch_weather(location, args.format)
+
+        if weather[0] != 200:
+            print('Unable to determine weather in {}'.format(location))
+        else:
+            loc = weather[1]['name']
+            temp = weather[1]['main']['temp']
+            unit = UNITS[args.format or 'imperial']
+            print(REPORT_FORMAT.format(loc, temp, unit))
+
 
 if __name__ == '__main__':
     main()
--- weather/weather.py
+++ weather/weather.py
@@ -5,29 +5,21 @@ from flask import Flask
 
 
 API_KEY = 'e7d99fbae935d84dafae9ba51bc49270'
+DEFAULT_WEATHER_UNITS = 'imperial'
 WEATHER_URL = 'https://api.openweathermap.org/data/2.5/weather'
-WEATHER_UNITS = 'imperial'
 
 
-def _process_weather_response(response):
-    """
-    Given a JSON blob, return the properly formatted weather
-    """
-    
-
-
-
-def fetch_weather(location):
+def fetch_weather(location, units=None):
     """
     Given a location, return the current weather in that location
     """
 
     payload = {
         'APPID': API_KEY,
-        'zip': location,
-        'units': WEATHER_UNITS
+        'zip': location ,
+        'units': units or DEFAULT_WEATHER_UNITS
     }
 
-    r = requests.get(WEATHER_URL, params=payload)
+    resp = requests.get(WEATHER_URL, params=payload)
 
-    return r.json()
+    return resp.status_code, resp.json()
