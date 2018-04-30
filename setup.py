353ad0e0d467c69be3d795b9cbbf1359cfc9eaf8
Eric Cheatham <ericjcheatham@gmail.com>
2018-04-30 11:57:44 -0500

    update setup. bump version

--- setup.py
+++ setup.py
@@ -1,17 +1,23 @@
 from setuptools import setup, find_packages
 
-setup(
+
+setup_args = dict(
     name='weathersnake',
-    version='0.0.3',
+    version='0.0.4',
     description='Python tool to find the weather',
     author='Eric Cheatham',
-    packages=find_packages(),
+    packages=find_packages(exclude=['tests']),
     include_package_data=True,
     install_requires=[
         'googlemaps',
         'requests[security]',
     ],
     entry_points={
-        'console_scripts': ['weathersnake = weather.cli:main']
+        'console_scripts': [
+            'weathersnake = weather.cli:main'
+        ]
     }
 )
+
+if __name__ == '__main__':
+    setup(**setup_args)
